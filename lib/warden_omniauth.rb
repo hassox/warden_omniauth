98d4d525f78ba466c625d9f8c9e7236c29257208
Uchio Kondo <udzura@udzura.jp>
2011-11-09 15:32:02 +0900

    add env['omniauth.auth'] guard on authenticate

--- lib/warden_omniauth.rb
+++ lib/warden_omniauth.rb
@@ -66,7 +66,7 @@ class WardenOmniAuth
 
       # set the user if one exists
       # otherwise, redirect for authentication
-      if user = (env['rack.auth'] || request['auth']) # TODO: Fix..  Completely insecure... do not use this will look in params for the auth.  Apparently fixed in the new gem
+      if user = (env['omniauth.auth'] || env['rack.auth'] || request['auth']) # TODO: Fix..  Completely insecure... do not use this will look in params for the auth.  Apparently fixed in the new gem
 
         success! self.class.on_callback[user]
       else
